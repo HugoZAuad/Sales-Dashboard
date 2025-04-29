13e6453eff124e1c78df0cdebff67cbf6b15f412
HugoZAuad <hugozeymer@hotmail.com>
2025-04-29 17:20:24 -0300

    Feat: table component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Header, CardComponent, AvatarList } from '@/components'
+import { Header, CardComponent, AvatarList, CustomTable } from '@/components'
 import { Container } from '@mui/material'
 import { currencyConverter } from '@/utils'
 
@@ -20,6 +20,27 @@ function Home() {
       subtitle: currencyConverter(6000.0),
     },
   ]
+
+  const mockTableData = {
+    headers: ['Name', 'Email', 'Actions'],
+    rows: [
+      [
+        <span>Nome 1</span>,
+        <span>nome1@email.com</span>,
+        <button>Action</button>,
+      ],
+      [
+        <span>Nome 2</span>,
+        <span>nome2@email.com</span>,
+        <button>Action</button>,
+      ],
+      [
+        <span>Nome 3</span>,
+        <span>nome3@email.com</span>,
+        <button>Action</button>,
+      ],
+    ],
+  }
   return (
     <>
       <Header />
@@ -27,6 +48,12 @@ function Home() {
         <CardComponent>CARD</CardComponent>
         <CardComponent>
           <AvatarList listData={mockListData} />
+        </CardComponent>
+        <CardComponent>
+          <CustomTable
+            headers={mockTableData.headers}
+            rows={mockTableData.rows}
+          />
         </CardComponent>
       </Container>
     </>
---
+++ src/types/customTable.d.ts
@@ -0,0 +1,4 @@
+export interface customTableProps {
+  headers: string[]
+  rows: React.ReactNode[][]
+}
