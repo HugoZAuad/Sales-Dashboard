cd007d8e671689f6240f4e30faddf362352d2d3e
HugoZAuad <hugozeymer@hotmail.com>
2025-05-06 01:12:25 -0300

    feat: continue create profile flow

--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -1,3 +1,6 @@
+import { ChangeEvent } from 'react'
+
+// Component
 import { Box, Container, Grid } from '@mui/material'
 import {
   BannerImage,
@@ -7,9 +10,61 @@ import {
   StyledUl,
   Logo,
 } from '@/components'
+
+//Utils
 import { pxToRem } from '@/utils'
 
+//Hook
+import { useFormValidation, usePost } from '@/hooks'
+
+//Redux
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from '@/redux'
+import { setProfileData } from '@/redux/slices/createProfile'
+
+//Types
+import { InputProps } from '@/types'
+
 function Registration() {
+  const dispatch = useDispatch()
+  const { email } = useSelector((state: RootState) => state.createProfile)
+
+  //Form Step1
+  const step1Inputs: InputProps[] = [
+    { name: 'name', type: 'text', placeholder: 'Nome', required: true },
+    { name: 'email', type: 'email', placeholder: 'E-mail' },
+    { name: 'phone', type: 'tel', placeholder: 'Telefone', required: true },
+  ]
+  const handleStep1 = (e: React.FormEvent) => {
+    e.preventDefault()
+    dispatch(
+      setProfileData({
+        email: String(step1FormValues[1]),
+      })
+    )
+  }
+
+  const {
+    formValues: step1FormValues,
+    formValid: step1FormValid,
+    handleChange: step1FormHandleChange,
+  } = useFormValidation(step1Inputs)
+
+  //Form Step2
+  const step2Inputs: InputProps[] = [{ type: 'password', placeholder: 'Senha' }]
+
+  const handleStep2 = async (e: React.FormEvent) => {
+    e.preventDefault()
+  }
+
+  const {
+    formValues: step2FormValues,
+    formValid: step2FormValid,
+    handleChange: step2FormHandleChange,
+  } = useFormValidation(step2Inputs)
+
+  const handleStepInputs = email ? step2Inputs : step1Inputs
+
   return (
     <>
       <Box>
@@ -25,27 +80,50 @@ function Registration() {
                 <Logo height={41} width={100} />
               </Box>
               <Box sx={{ marginBttom: pxToRem(24) }}>
-                <StyledH1>Faça seu cadastro</StyledH1>
-                <StyledP>Primeiro, diga-nos quem você é.</StyledP>
-                <StyledUl>
-                  <li>Entre 8 e 16 caracteres;</li>
-                  <li>Pelo menos uma letra maiúscula;</li>
-                  <li>Pelo menos um caractere especial.</li>
-                  <li>Pelo menos um número</li>
-                </StyledUl>
+                <StyledH1>
+                  {email ? 'Defina sua senha' : 'Faça o seu cadastro'}
+                </StyledH1>
+                <StyledP>
+                  {email
+                    ? 'sua senha deve ter:'
+                    : 'Primeiro diga-nos quem você é'}
+                </StyledP>
+                {email && (
+                  <StyledUl>
+                    <li>Entre 8 e 16 caracteres;</li>
+                    <li>Pelo menos uma letra maiúscula;</li>
+                    <li>Pelo menos um caractere especial.</li>
+                    <li>Pelo menos um número</li>
+                  </StyledUl>
+                )}
               </Box>
               <FormComponent
-                inputs={[
-                  { type: 'email', placeholder: 'Email' },
-                  { type: 'password', placeholder: 'senha' },
-                ]}
+                inputs={handleStepInputs.map((input, index) => ({
+                  type: input.type,
+                  placeholder: input.placeholder,
+                  value: email
+                    ? step2FormValues[index] || ''
+                    : step1FormValues[index] || '',
+                  onChange: (e: ChangeEvent<HTMLInputElement>) =>
+                    email
+                      ? step2FormHandleChange(
+                          index,
+                          (e.target as HTMLInputElement).value
+                        )
+                      : step1FormHandleChange(
+                          index,
+                          (e.target as HTMLInputElement).value
+                        ),
+                }))}
                 buttons={[
-                  { className: 'primary', type: 'submit', children: 'Login' },
+                  {
+                    className: 'primary',
+                    disabled: email ? !step2FormValid : !step1FormValid,
+                    onClick: email ? handleStep2 : handleStep1,
+                    type: 'submit',
+                    children: email ? 'Enviar' : 'Próximo',
+                  },
                 ]}
-                message={{
-                  msg: 'Erro!',
-                  type: 'error',
-                }}
               />
             </Container>
           </Grid>
